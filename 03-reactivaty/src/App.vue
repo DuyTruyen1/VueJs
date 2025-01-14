fac26b78eaa2048446c9d9083c4f41d11bee83ef
DuyTruyen1 <truyenmap420@gmail.com>
2025-01-14 13:20:27 +0700

    Deep Reactivity

---
+++ 03-reactivaty/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { reactive , ref } from 'vue';
+// dùng ref thì phải bắt buộc có value
+// dùng reactive thì không  cần phải chấm đến value
+ const object = reactive({
+  count:2,
+  user:{
+    age:18
+  },
+  arr: [1,2,3]
+ })
+
+ const change = () => {
+  object.value.count++
+  object.value.user.age++
+  object.value.arr.push(4)
+ }
+</script>
+
+<template>
+  <div> = 
+    <p>Count: {{ object.count }}</p>
+    <p>Age: {{ object.user.age }}</p>
+    <p>Age: {{ object.arr }}</p>
+  <button @click="change">Increase</button>
+  </div>
+</template>
